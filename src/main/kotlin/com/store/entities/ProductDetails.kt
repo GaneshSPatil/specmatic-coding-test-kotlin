f971d025e19e7851d48a2c19cdf2862e974b3218
Ganesh Patil <ganeshp8996@gmail.com>
2025-03-10 09:17:50 +0530

    Achieve interface delegation by defining abstract properties in the interface

--- src/main/kotlin/com/store/entities/ProductDetails.kt
+++ src/main/kotlin/com/store/entities/ProductDetails.kt
@@ -5,17 +5,24 @@ import jakarta.validation.constraints.Max
 import jakarta.validation.constraints.Min
 import jakarta.validation.constraints.NotNull
 
+interface ProductDetailsInfo {
+    val name: String
+    val type: ProductType
+    val inventory: Int
+    val cost: Double?
+}
+
 data class ProductDetails(
     @TextOnly
-    val name: String,
+    override val name: String,
 
     @field:NotNull
-    val type: ProductType,
+    override val type: ProductType,
 
     @field:Min(1)
     @field:Max(9999)
-    val inventory: Int,
+    override val inventory: Int,
 
     @field:NotNull
-    val cost: Double? = 0.0,
-)
+    override val cost: Double? = 0.0,
+) : ProductDetailsInfo
